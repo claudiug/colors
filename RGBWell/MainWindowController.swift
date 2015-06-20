59b2e65f6eadda0d85c88c5f5b956074d6708cca
claudiug <claudiu.garba@gmail.com>
2015-06-20 21:08:55 +0200

    add crtl

---
+++ RGBWell/MainWindowController.swift
@@ -0,0 +1,14 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by bender on 6/20/15.
+//  Copyright © 2015 claudiucancode. All rights reserved.
+//
+
+import Cocoa
+
+
+class MainWindowController: NSWindowController {
+    
+}
